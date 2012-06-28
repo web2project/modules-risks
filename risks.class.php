fe340bfc87a88059341d684db7430193a1ffab61
D Keith Casey Jr <keith@caseysoftware.com>
2012-06-28 01:26:16 -0500

    added the risk_owner field

--- risks.class.php
+++ risks.class.php
@@ -82,6 +82,10 @@ class CRisk extends w2p_Core_BaseObject {
         $q->addQuery('p.project_id, p.project_name, p.project_color_identifier, p.project_company');
         $q->leftJoin('projects', 'p', 'p.project_id = r.risk_project');
 
+        $q->addQuery('contact_display_name as risk_owner');
+        $q->leftJoin('users', 'u', 'risk_owner = user_id');
+        $q->leftJoin('contacts', 'c', 'user_contact = contact_id');
+
         $projObj = new CProject();
         $projObj->setAllowedSQL($this->_AppUI->user_id, $q, null, 'p');
         if ($project_id > 0 && $this->_perms->checkModuleItem('projects', 'view', $project_id)) {
