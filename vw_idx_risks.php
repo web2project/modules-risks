836a736c96fcd5f65faeeffe83166db99dbc93ef
D Keith Casey Jr <keith@caseysoftware.com>
2012-06-28 01:26:32 -0500

    wired flexifields into the list view;

--- vw_idx_risks.php
+++ vw_idx_risks.php
@@ -8,26 +8,44 @@ global $AppUI, $task_id, $tab;
 $project_id = (int) w2PgetParam($_GET, 'project_id', 0);
 
 $tab--;
-$df = $AppUI->getPref('SHDATEFORMAT');
-$riskProbability = w2PgetSysVal( 'RiskProbability' );
-$riskImpact = w2PgetSysVal( 'RiskImpact' );
-$riskStatus = w2PgetSysVal( 'RiskStatus' );
 
 $risk = new CRisk();
 $risks = $risk->getRisksByProject($project_id, $tab--);
+
+$module = new w2p_Core_Module();
+$fields = $module->loadSettings('risks', 'index_list');
+
+if (count($fields) > 0) {
+    $fieldList = array_keys($fields);
+    $fieldNames = array_values($fields);
+} else {
+    // TODO: This is only in place to provide an pre-upgrade-safe 
+    //   state for versions earlier than v3.0
+    //   At some point at/after v4.0, this should be deprecated
+    $fieldList = array('risk_name', 'risk_priority', 'risk_task', 'risk_probability',
+        'risk_impact', 'risk_owner', 'risk_status', 'risk_mitigation_date');
+    $fieldNames = array('Name', 'Priority', 'Related Task', 'Probability',
+        'Impact', 'Owner', 'Status', 'Mitigation Date');
+
+    $module->storeSettings('risks', 'index_list', $fieldList, $fieldNames);
+}
+
+$htmlHelper = new w2p_Output_HTMLHelper($AppUI);
+
+$riskProbability = w2PgetSysVal( 'RiskProbability' );
+$riskImpact = w2PgetSysVal( 'RiskImpact' );
+$riskStatus = w2PgetSysVal( 'RiskStatus' );
+$customLookups = array('risk_probability' => $riskProbability, 
+    'risk_impact' => $riskImpact, 'risk_status' => $riskStatus);
 ?>
-<table border="0" width="100%" cellspacing="1" cellpadding="2" class="tbl">
-	<tr bgcolor="#99CCFF" align="center" valign="top">
-        <th align="center" valign="top" class="hdr" width="200px;"><?php echo $AppUI->_('Name'); ?></th>
-        <th align="center" valign="top" class="hdr" width="25px;"><?php echo $AppUI->_('Priority'); ?></th>
-		<th align="center" valign="top" class="hdr" width="25px;"><?php echo $AppUI->_('Related Task'); ?></th>
-		<th align="center" valign="top" class="hdr" width="20px;"><?php echo $AppUI->_('Probability'); ?></th>
-		<th align="center" valign="top" class="hdr" width="10px;"><?php echo $AppUI->_('Impact'); ?></th>
-		<th align="center" valign="top" class="hdr" width="10px;"><?php echo $AppUI->_('Owner'); ?></th>
-        <th align="center" valign="top" class="hdr" width="10px;"><?php echo $AppUI->_('Status'); ?></th>
-		<th align="center" valign="top" class="hdr" width="10px;"><?php echo $AppUI->_('Mitigation Date'); ?></th>
-	</tr>
+<table width="100%" border="0" cellpadding="2" cellspacing="1" class="tbl list">
     <?php
+    foreach ($fieldNames as $index => $name) { ?>
+        <th nowrap="nowrap">
+            <?php echo $AppUI->_($fieldNames[$index]); ?>
+        </th>
+    <?php }
+
 
     $prev_project = -1;
     foreach ($risks as $row) {
@@ -37,38 +55,15 @@ $risks = $risk->getRisksByProject($project_id, $tab--);
                     <font color="' . bestColor( $row["project_color_identifier"] ) . '">' . $row['project_name'] . '</font>&nbsp</a>
                 </td></tr>';
         }
-        $row['risk_status'] = $riskStatus[$row['risk_status']];
-        ?>
-        <tr>
-            <td>
-                <a href="?m=risks&amp;a=view&amp;risk_id=<?php echo $row['risk_id']; ?>"><?php echo $row['risk_name']; ?></a>
-            </td>
-            <td style="text-align: center;"><?php echo $row['risk_priority']; ?>&nbsp</td>
-            <td>
-                <?php if ($row['task_id'] > 0) { ?>
-                <a href="?m=tasks&amp;a=view&amp;task_id=<?php echo $row['task_id']; ?>">
-                    <?php echo $row['task_name']; ?>
-                </a>
-                <?php } else { ?>
-                    <?php echo $AppUI->_('No task specified'); ?>
-                <?php } ?>
-            </td>
-            <td nowrap="nowrap" style="text-align: center;"><?php echo $riskProbability[$row['risk_probability']]; ?> &nbsp</td>
-            <td nowrap="nowrap" style="text-align: center;"><?php echo $riskImpact[$row['risk_impact']]; ?> &nbsp</td>
-            <td nowrap="nowrap" style="text-align: center;">
-                <?php echo $row['owner_name']; ?>
-            </td>
-            <td nowrap="nowrap" style="text-align: center;">
-                <?php echo $row['risk_status']; ?>
-            </td>
-            <td nowrap="nowrap" style="text-align: center;">
-                <?php
-                    $mitigationDate = intval($row["risk_mitigation_date"]) ? new CDate( $row["risk_mitigation_date"] ) : null;
-                    echo $mitigationDate ? $mitigationDate->format( $df ) : '-';
-                ?> &nbsp
-            </td>
-        </tr>
-        <?php
+//TODO: center probability and impact
+//TODO: when no task is specified, say so
+//TODO: make sure mitigation date works
+        echo '<tr>';
+        $htmlHelper->stageRowData($row);
+        foreach ($fieldList as $index => $column) {
+            echo $htmlHelper->createCell($fieldList[$index], $row[$fieldList[$index]], $customLookups);
+        }
+        echo '</tr>';
         $prev_project = $row['project_id'];
     }
     ?>
